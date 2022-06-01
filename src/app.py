74185bf9b6ba9a8d7503faba6b3711f3ca719d70
giorgos547 <giorgos54705@gmail.com>
2022-06-02 02:06:45 +0300

    Created Signup page

--- src/app.py
+++ src/app.py
@@ -31,5 +31,9 @@ def subscription():
 def contact():
     return render_template("contact.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
